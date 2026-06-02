67ccd88d4f9e159cc60fe85d5919d25b09723531
CristianJavi <CristianColina_JavierBarragan@outlook.es>
2026-06-02 12:55:39 +0200

    quitado comentario

--- src/main/java/com/practica/ems/covid/Principal.java
+++ src/main/java/com/practica/ems/covid/Principal.java
@@ -26,6 +26,5 @@ public class Principal {
 		System.out.println(contactosCovid.getListaContactos());
 		FechaHora ini = new FechaHora(25,5,2021,16,30);
 		FechaHora fin = new FechaHora(25,5,2021,16,30);
-		//prueba1
 	}
 }
